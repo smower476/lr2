1fcaed3e7aa5fe0956078e410a1aa3c5f58bd114
Anton <45099803+MrAntonS@users.noreply.github.com>
2024-09-19 17:18:03 -0400

    Добавил функцию вывода сформированной цены товара

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 class Coating():
+    colors = ["Белый", "Синий", "Желтый", "Красный", "Перламутровый", "Серый металлик"]
+    parts = ["Капот", "Передняя дверь", "Задняя дверь", "Передний бампер", "Задний бампер", "Крыша"]
     def __init__(self):
         self.part = None
         self.color = None
+        self.price = None
         self.cost = 0
     def get_info(self):
         pass
     def output_price(self):
-        pass
+        print(f"Цена покраски детали \"{self.part}\" в {self.color} составляет {self.price} рублей")
     def part_coefficient(self):
         pass
 
 if __name__ == '__main__':
+    a1 = Coating()
+    a2 = Coating()
     pass
