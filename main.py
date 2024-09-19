51d559cb5142853c31079ece4da5b6f30a130ec7
Chicken <121474491+smower476@users.noreply.github.com>
2024-09-20 00:20:10 +0300

    Работают над вводом в программу

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ class Coating():
         self.color = None
         self.cost = 0
     def get_info(self):
-        pass
+        self.part = input(f"Введите название детали, Например {self.parts}: ")
+        self.color = input("Введите цвет детали, Например: {self.colors}: ")
+        
     def output_price(self):
         pass
     def part_coefficient(self):
@@ -12,3 +14,5 @@ class Coating():
 
 if __name__ == '__main__':
     pass
+
+
