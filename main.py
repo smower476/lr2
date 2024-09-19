9ad6a898f550ca4eca5f1dedeb43b1143900c272
Anton <45099803+MrAntonS@users.noreply.github.com>
2024-09-19 17:02:43 -0400

    Добавил Абстракцию

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+class Coating():
+    def __init__(self):
+        self.part = None
+        self.color = None
+        self.cost = 0
+    def get_info(self):
+        pass
+    def output_price(self):
+        pass
+    def part_coefficient(self):
+        pass
 
-
+if __name__ == '__main__':
+    pass
