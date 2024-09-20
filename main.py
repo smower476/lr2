3baf99b7e02f507300c6de147be8de7c15425cd1
ShNzofrenia <d4nilazaremba@yandex.ru>
2024-09-20 09:16:12 +0300

    Добавил функцию расчет коэффициента

--- main.py
+++ main.py
@@ -13,12 +13,35 @@ class Coating():
     def output_price(self):
         print(f"Цена покраски детали \"{self.part}\" в {self.color} составляет {self.price} рублей")
     def part_coefficient(self):
-        '''
-        TODO: Записать финальную цену в self.price
-        '''
-        pass
+        color_coefficients = {
+            "Белый": 1,
+            "Синий": 1,
+            "Желтый": 1.1,
+            "Красный": 1,
+            "Перламутровый": 1.2,
+            "Серый металлик": 1.3
+        }
+
+        part_coefficients = {
+            "Капот": 1,
+            "Передняя дверь": 1.2,
+            "Задняя дверь": 1.1,
+            "Передний бампер": 1,
+            "Задний бампер": 1,
+            "Крыша": 1.1
+        }
+
+        base_price = 12000  
+
+
+        color_coef = color_coefficients.get(self.color, 1)
+        part_coef = part_coefficients.get(self.part, 1)
+
+
+        self.price = base_price * color_coef * part_coef
 
 if __name__ == '__main__':
     a1 = Coating()
     a1.get_info()
+    a1.part_coefficient()
     a1.output_price()
